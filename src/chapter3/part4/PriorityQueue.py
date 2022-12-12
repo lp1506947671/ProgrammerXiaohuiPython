8da11d6c409ddac616f47b2a6c46dc42f9cea5bf
lp1506947671 <xiaopawnye@163.com>
2022-12-12 21:48:30 +0800

    [类型]:需求开发 [描述]:优先队列优化 [提交人]:Jason

--- src/chapter3/part4/PriorityQueue.py
+++ src/chapter3/part4/PriorityQueue.py
@@ -2,27 +2,23 @@ class PriorityQueue:
 
     def __init__(self):
         self.array = []
-        self.size = 0
 
     def enqueue(self, element):
         self.array.append(element)
-        self.size += 1
         self.up_adjust()
 
     def dequeue(self):
-        if self.size < 0:
+        if not self.array:
             raise Exception("队列为空 !")
         head = self.array[0]
-        self.array[0] = self.array[self.size-1]
-        self.size -= 1
+        self.array[0] = self.array[len(self.array) - 1]
         self.array.pop()
-        if not self.array:
-            return head
-        self.down_adjust()
+        if self.array:
+            self.down_adjust()
         return head
 
     def up_adjust(self):
-        child_index = self.size - 1
+        child_index = len(self.array) - 1
         parent_index = (child_index - 1) // 2
         # temp保存插入的叶子节点值，用于最后的赋值
         temp = self.array[child_index]
@@ -38,9 +34,9 @@ class PriorityQueue:
         # temp保存父节点值，用于最后的赋值
         temp = self.array[parent_index]
         child_index = 1
-        while child_index < self.size:
+        while child_index < len(self.array):
             # 如果有右孩子，且右孩子大于左孩子的值，则定位到右孩子
-            if child_index + 1 < self.size and self.array[child_index + 1] > self.array[child_index]:
+            if child_index + 1 < len(self.array) and self.array[child_index + 1] > self.array[child_index]:
                 child_index += 1
             # 如果父节点大于任何一个孩子的值，直接跳出
             if temp >= self.array[child_index]:
@@ -64,4 +60,3 @@ queue.enqueue(6)
 queue.enqueue(7)
 print(queue.dequeue())
 print(queue.dequeue())
-

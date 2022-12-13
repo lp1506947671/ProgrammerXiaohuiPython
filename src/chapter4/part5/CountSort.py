d0c12ce46b08e0000e5d1a53725bb35870ed4991
lp1506947671 <xiaopawnye@163.com>
2022-12-13 08:27:03 +0800

    [类型]:需求开发 [描述]:计数排序优化 [提交人]:Jason

--- src/chapter4/part5/CountSort.py
+++ src/chapter4/part5/CountSort.py
@@ -18,29 +18,31 @@ def count_sort(array=[]):
     return sorted_array
 
 
-def count_sort_v2(array=[]):
-    # 1.得到数列的最大值最小值，并算出差值d
+def count_sort(array=None):
+    if not array:
+        return []
     max_value = array[0]
     min_value = array[0]
-    for i in range(1, len(array)):
-        if array[i] > max_value:
-            max_value = array[i]
-        if array[i] < min_value:
-            min_value = array[i]
-    d = max_value - min_value
+    # 1.获取最大值和最小值
+    for item in array[1:]:
+        if item > max_value:
+            max_value = item
+        if item < min_value:
+            min_value = item
     # 2.创建统计数组并统计对应元素个数
-    count_array = [0] * (d+1)
-    for i in range(0, len(array)):
-        count_array[array[i]-min_value] += 1
-    # 3.统计数组做变形，后面的元素等于前面的元素之和
-    for i in range(1, len(count_array)):
-        count_array[i] += count_array[i-1]
-    # 4.倒序遍历原始数列，从统计数组找到正确位置，输出到结果数组
-    sorted_array = [0] * len(array)
-    for i in range(len(array)-1, -1, -1):
-        sorted_array[count_array[array[i]-min_value]-1] = array[i]
-        count_array[array[i] - min_value] -= 1
-    return sorted_array
+    count_array = [0] * (max_value - min_value + 1)
+    for item in array:
+        count_array[item - min_value] += 1
+    # 3.统计数组做变形,后面的元素等于前面的元素之和
+    for index in range(1, len(count_array)):
+        count_array[index] += count_array[index - 1]
+    # 4.倒序遍历原始数列,从统计数组找到正确位置，输出到结果数组
+    result = [0] * len(array)
+    for item in array[::-1]:
+        result[count_array[item - min_value] - 1] = item
+        count_array[item - min_value] -= 1
+    return result
+
 
 
 my_array = list([4, 4, 6, 5, 3, 2, 8, 1, 7, 5, 6, 0, 10])
